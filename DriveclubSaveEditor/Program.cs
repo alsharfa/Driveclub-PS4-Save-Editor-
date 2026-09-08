205aed0aa2fddb12b1a7328327d8bbfe4795fae6
alsharfa <mohammedar@btinternet.com>
2026-09-08 19:46:31 +0100

    Add application entry point

---
+++ DriveclubSaveEditor/Program.cs
@@ -0,0 +1,11 @@
+namespace DriveclubSaveEditor;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
